54ae122e8768e19583ae9919598273ea63b0e677
Zejun Yuan <zyuan11@ucsc.edu>
2017-04-10 20:51:09 -0700

    particle system working from sfml webpage

---
+++ ParticleSystems/ParticleSystems/main.cpp
@@ -0,0 +1,41 @@
+#include <SFML/Graphics.hpp>
+#include "ParticleSystem.cpp"
+
+int main()
+{
+	// create the window
+	sf::RenderWindow window(sf::VideoMode(512, 256), "Particles");
+
+	// create the particle system
+	ParticleSystem particles(1000);
+
+	// create a clock to track the elapsed time
+	sf::Clock clock;
+
+	// run the main loop
+	while (window.isOpen())
+	{
+		// handle events
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+
+		// make the particle system emitter follow the mouse
+		sf::Vector2i mouse = sf::Mouse::getPosition(window);
+		particles.setEmitter(window.mapPixelToCoords(mouse));
+
+		// update it
+		sf::Time elapsed = clock.restart();
+		particles.update(elapsed);
+
+		// draw it
+		window.clear();
+		window.draw(particles);
+		window.display();
+	}
+
+	return 0;
+}
